26b5376ad9c89dbfe0c9c5258cccf4ee67a12b1f
krongkarn.jitsil <krongkarn.jitsil@contentsquare.com>
2026-03-24 01:00:46 +0100

    update CV making font smoother

--- src/pages/Home.react.tsx
+++ src/pages/Home.react.tsx
@@ -18,7 +18,7 @@ export const Home = () => {
               <span>my cv page</span>
             </Button>
             <Button
-              href='https://www.dropbox.com/scl/fi/1g4ieopdmfcxey97l7vqb/Krongkarn-JITSIL-CV-2026.pdf?rlkey=abys80myxdmmy6pgpiagb1zbu&st=rjnbauus&dl=0'
+              href='https://www.dropbox.com/scl/fi/r8174auqll5g1t93u2zp7/Krongkarn-JITSIL-CV-2026.pdf?rlkey=dq5yze0yo5l0ia4xfvjyla9sx&st=zfsbxc2c&dl=0'
             >
               <span>view full cv as pdf</span>
             </Button>
